52633eca04507be653f7be582c3ed2ecb10faddc
Vagabond <Vagabond@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2016-03-22 23:59:55 +0000

    OP2Helper: Forgot to add Bulldozer.h/Bulldoze.cpp in last commit.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/OP2Helper@1072 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

---
+++ Bulldozer.cpp
@@ -0,0 +1,62 @@
+#include "Bulldozer.h"
+#include <iterator>
+
+namespace Bulldozer
+{
+	const int FirstRockTileIndex = 439;
+	const int FirstSandTileIndex = 1206;
+
+	const int DozedMud = 414;
+	const int DozedRock = 921;
+	const int DozedSand = 1670;
+
+	bool IsDozeable(const LOCATION &location)
+	{
+		int currentCellType = GameMap::GetCellType(location);
+
+		return
+			currentCellType == CellTypes::cellFastPassible1 ||
+			currentCellType == CellTypes::cellFastPassible2 ||
+			currentCellType == CellTypes::cellMediumPassible1 ||
+			currentCellType == CellTypes::cellMediumPassible2 ||
+			currentCellType == CellTypes::cellSlowPassible1 ||
+			currentCellType == CellTypes::cellSlowPassible2;
+	}
+
+	void Doze(const LOCATION &loc)
+	{
+		if (!IsDozeable(loc))
+		{
+			return;
+		}
+
+		int originalTileIndex = GameMap::GetTile(loc);
+
+		if (originalTileIndex < FirstRockTileIndex)
+		{
+			GameMap::SetTile(loc, DozedMud);
+		}
+		else if (originalTileIndex < FirstSandTileIndex)
+		{
+			GameMap::SetTile(loc, DozedRock);
+		}
+		else
+		{
+			GameMap::SetTile(loc, DozedSand);
+		}
+
+		GameMap::SetCellType(loc, CellTypes::cellDozedArea);
+	}
+
+	void Doze(const MAP_RECT &mapRect)
+	{
+		for (int y = 0; y < mapRect.Height(); ++y)
+		{
+			for (int x = 0; x < mapRect.Width(); ++x)
+			{
+				LOCATION dozeLoc(mapRect.x1 + x, mapRect.y1 + y);
+				Doze(dozeLoc);
+			}
+		}
+	}
+}
---
+++ Bulldozer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Outpost2DLL\Outpost2DLL.h"
+
+namespace Bulldozer
+{
+	// Checks if a LOCATION on the map can be bulldozed. 
+	bool IsDozeable(const LOCATION &loc);
+
+	// Dozes a LOCATION only if LOCATION is valid to doze.
+	void Doze(const LOCATION &loc);
+
+	// Dozes all LOCATIONs contained in MAP_RECT that are valid to doze.
+	void Doze(const MAP_RECT &mapRect);
+}
