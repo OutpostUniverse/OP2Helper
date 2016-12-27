ef7ffc5bdfd8477cc60ee459e33c05d30121c3b4
Vagabond <Vagabond@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2016-12-27 05:41:56 +0000

    OP2Helper: Expanded to include more functions to ease initialization of volcanoes and lava flows. See Lava.h/Lava.cpp. NOTE: This causes name collisions in existing scenario projects.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/OP2Helper@1124 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- Lava.h
+++ Lava.h
@@ -1,9 +1,27 @@
 #pragma once
 
+#include "Outpost2DLL\Outpost2DLL.h"
+#include "OP2Helper\OP2Helper.h"
 
-// Note: This file contains code to emulate how official Sierra maps seem to be built.
-// These structs are used to define data about where lava can expand on the map.
+// Sets all tiles in a MAP_RECT to lava-possible. 
+void SetLavaPossibleRegion(const MAP_RECT& mapRect);
 
+// Sets all S1 and S2 Celltypes in a MAP_RECT to lava-possible 
+void SetLavaPossibleAllSlowCells(const MAP_RECT& mapRect);
+
+//Functions to start the lava flow animation on the side of a volcano
+void AnimateFlowSW(const LOCATION& TopLeftMostTile);
+void AnimateFlowS(const LOCATION& TopLeftMostTile);
+void AnimateFlowSE(const LOCATION& TopLeftMostTile);
+
+//Functions to stop the lava flow animation on the side of a volcano
+void FreezeFlowSW(const LOCATION& TopLeftMostTile);
+void FreezeFlowS(const LOCATION& TopLeftMostTile);
+void FreezeFlowSE(const LOCATION& TopLeftMostTile);
+
+
+// Note: The structs Range and LavaPossibleInfo emulate how official Sierra maps 
+//       define data about where lava can expand on the map.
 
 // Size: 0x8
 struct Range
