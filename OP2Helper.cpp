b43dd03172136ccfea6a7fc10ef47257cd1270d5
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-05-03 20:58:36 -0400

    Add += and -= operator overloads for LOCATION

--- OP2Helper.cpp
+++ OP2Helper.cpp
@@ -204,6 +204,20 @@ LOCATION operator- (const LOCATION &loc1, const LOCATION &loc2) {
 	return LOCATION(loc1.x - loc2.x, loc1.y - loc2.y);
 }
 
+LOCATION& operator+= (LOCATION& loc1, const LOCATION& loc2) {
+	loc1.x += loc2.x;
+	loc1.y += loc2.y;
+
+	return loc1;
+}
+
+LOCATION& operator-= (LOCATION& loc1, const LOCATION& loc2) {
+	loc1.x -= loc2.x;
+	loc1.y -= loc2.y;
+
+	return loc1;
+}
+
 bool operator== (const LOCATION& loc1, const LOCATION &loc2) {
 	return (loc1.x == loc2.x && loc1.y == loc2.y);
 }
